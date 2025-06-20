094275f15b3214ea84ee2b15a45fbb90ab8f330d
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2025-06-20 23:13:24 +0530

    Time: 79 ms (28.02%), Space: 60.2 MB (65.56%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,29 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+         //create STACK
         Stack<Integer>helperStack=new Stack<Integer>();
 
-        int n=temperatures.length;
-        int[]result=new int[n];
+        int n=temperatures.length;//length of temp
+        int[]result=new int[n];//create array to keep distance and need to return it
 
-       for(int idx=n-1;idx>=0;idx--){
+       for(int idx=n-1;idx>=0;idx--){//iterating from REVERSE
+              
 
+              //pop ele is idx with a lower or equal
+              //temp than the curr idx
           while(!helperStack.isEmpty() && temperatures[idx]>=temperatures[helperStack.peek()]){
             helperStack.pop(); //remove it
           }
-
+            
+            //if stack still has ele
+            //then next warm temp exists
           if(!helperStack.isEmpty()){
-            result[idx]=helperStack.peek()-idx;
+            result[idx]=helperStack.peek()-idx;//to get distance Diff of idx
           }
-
+           
+           //inserting curr idx in stack
           helperStack.push(idx);
        }
-       return result;
+       return result;//returing the array which has diatance
     }
 }

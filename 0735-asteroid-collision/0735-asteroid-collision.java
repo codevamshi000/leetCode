f4f350f51404c20179b59b8ff9c3d0b41eadfaa0
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2025-06-22 23:04:54 +0530

    Time: 4 ms (94.51%), Space: 45.5 MB (34.6%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,10 +1,12 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        //create stack
         Stack<Integer>stack=new Stack<Integer>();
-
+        //ioop iterate into giving array
         for(int asteroid : asteroids){
-            boolean destroyed=false;
+            boolean destroyed=false;//to track
 
+         //conditions of collisions
         while(!stack.isEmpty() && asteroid <0 &&stack.peek() >0){
             if(stack.peek() < -asteroid){
                 stack.pop();

910d40f68db26e2ce31f304891cd64e2a60bcc8c
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2025-07-01 09:55:45 +0530

    Time: 1 ms (87.19%), Space: 42.7 MB (7.44%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,17 +1,20 @@
 class Solution {
     public int possibleStringCount(String word) {
         int n=word.length();
-        int total=1;
+        int total=1;//already word is 1 output from 5 so find another 4 possible outcome
 
         int i=0;
         while(i<n){
             char ch=word.charAt(i);
             int count=0;
-
+         
+         //counting how many times one char repated
             while(i<n && word.charAt(i)==ch){
                 count++;
                 i++;
             }
+//if count in more then 1 time then she accidently typed too long many key
+//we use (count-1)by adding the giving word to total so 1(already given)+4(total)=5 
             if(count>1){
                 total=total+(count-1);
             }

96f05d15867a87fa707c9c68c6c735996218226a
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2025-08-08 15:12:43 +0530

    Time: 2 ms (97.61%), Space: 45.7 MB (62.19%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -15,23 +15,32 @@
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        List<Double>result=new ArrayList<>();
-        if(root==null) return result;
+  //Create list to hold AVGs to store in decimal we use Double     
+        List<Double>averages=new ArrayList<>();
+        if(root==null) return averages;
 
-        Queue<TreeNode>q=new LinkedList<>();
-        q.offer(root);
+//"Use a queue for BFS(level-order traversal); start by adding the root."
+        Queue<TreeNode>queue=new LinkedList<>();
+        queue.offer(root);
 
-        while(!q.isEmpty()){
-            int size=q.size();
-            long sum=0L;
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();
-                sum+=node.val;
-                if(node.left !=null)q.offer(node.left);
-                if(node.right !=null)q.offer(node.right);
-            }
-            result.add(sum/(double)size);
-        }
-        return result;
+//looping 
+        while(!queue.isEmpty()){
+  //At the start of a level, levelSize is the number of nodes on that level.          
+          int levelSize=queue.size(); 
+          double sum=0;
+
+//Loop through all nodes that belong to this level.
+          for(int i=0;i<levelSize;i++){
+//"Remove the front node; poll() is safe since levelSize ensures enough elements."            
+            TreeNode current=queue.poll();
+            sum += current.val;//add this node’s value to the level sum.
+
+//enqueue left & right child for the next level (if present).
+            if(current.left !=null) queue.add(current.left);
+            if(current.right !=null) queue.add(current.right);
+          }
+          averages.add(sum/levelSize);//take the avg of them
     }
+    return averages;
+    } 
 }

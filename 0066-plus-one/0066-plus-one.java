ce40d96ab762fadd208172bfb81e73f0632d4d8f
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2025-11-24 15:07:38 +0530

    Time: 0 ms (100%), Space: 43.5 MB (10%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,18 +2,19 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n=digits.length;
 
-  // Traverse from last digit backwards
+ // Traverse from last digit backwards
         for(int i=n-1;i>=0;i--){
             if(digits[i]<9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i]=0;
+            digits[i]++;
+            return digits;
         }
-         // If control reaches here → number was like 999 → 1000
-         int[] newNumber=new int[n+1];
-         newNumber[0]=1;  // rest already 0 by default
+      digits[i]=0;
+    }
 
-         return newNumber;
+    // If control reaches here → number was like 999 → 1000
+    int []newNum=new int[n+1];
+    newNum[0]=1;   // rest already 0 by default
+    
+    return newNum;
     }
 }

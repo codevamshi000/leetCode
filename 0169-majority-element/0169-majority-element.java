a122c5859fb70e4021176c90d56690faa0e24c2d
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2025-11-28 21:03:23 +0530

    Time: 2 ms (75.09%), Space: 55.8 MB (10.37%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int can=0;
-        int point=0;
-        for(int i=0;i<nums.length;i++){
-            if(point==0){
-                can=nums[i];
-            }if(can==nums[i]){
+        int can = 0;     // candidate
+        int point = 0;   // count
+
+        for(int i = 0; i < nums.length; i++){
+            if(point == 0){
+                can = nums[i];
+            }
+            if(can == nums[i]){
                 point++;
-            }else{
+            } else {
                 point--;
             }
         }

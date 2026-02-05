9f499c7713ab535b685e3416d59ae2358643b42d
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2026-02-05 12:10:29 +0530

    Time: 1 ms (99.63%), Space: 94.3 MB (89.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,32 @@
 class Solution {
+    // Main class required by LeetCode
+
     public int maxProfit(int[] prices) {
-        int buy=prices[0];
-        int profit=0;
-        for(int i=0;i<prices.length;i++){
-            if(prices[i]<buy){
-              buy=prices[i];   
-            } else if(prices[i]-buy> profit){
-              profit=prices[i]-buy;
+        // Method to find maximum profit from one buy and one sell
+
+        int buy = prices[0];
+        // Store the minimum buying price (initially first day price)
+
+        int profit = 0;
+        // Store the maximum profit found so far
+
+        for (int i = 0; i < prices.length; i++) {
+            // Loop through each day's price
+
+            if (prices[i] < buy) {
+                // If current price is lower than buying price
+
+                buy = prices[i];
+                // Update buying price to lower value
+            } else if (prices[i] - buy > profit) {
+                // If selling today gives more profit than before
+
+                profit = prices[i] - buy;
+                // Update maximum profit
             }
         }
+
         return profit;
+        // Return the maximum profit
     }
 }

c64206b23364f16213dc3401c397302d9c535b42
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2026-02-06 16:56:15 +0530

    Time: 0 ms (100%), Space: 43.8 MB (6.43%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,12 +1,18 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k=0;
-      for(int i=0;i<nums.length;i++){
-        if(nums[i]!=val){
-           nums[k]=nums[i];
-           k++;             
+
+        int k = 0;   // index to place valid elements
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // if current element is NOT the one to remove
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // keep it
+                k++;               // move to next free spot
+            }
         }
-      }
+
+        // k = count of elements kept
         return k;
     }
 }

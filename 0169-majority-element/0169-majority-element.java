b75e2ec32ea80e3056bebc629e9508db82ae3607
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2026-02-09 08:24:14 +0530

    Time: 1 ms (99.83%), Space: 55.7 MB (45.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,11 +4,13 @@ class Solution {
         int point = 0;   // count
 
         for(int i = 0; i < nums.length; i++){
-            if(point == 0){
+            if(point == 0){//If count becomes zero, choose a new candidate.
                 can = nums[i];
-            }
+            } 
             if(can == nums[i]){
                 point++;
+                //If current element equals candidate → vote increases
+              //Else → vote decreases
             } else {
                 point--;
             }

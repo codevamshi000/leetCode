8eac9515dd44fedb7eede0760420823c10fc8d3b
Vamshi <157671424+codevamshi000@users.noreply.github.com>
2026-03-20 17:16:01 +0530

    Time: 0 ms (100%), Space: 46.8 MB (37.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,11 +1,21 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       int i=0;
-       for(int j=0;j<nums.length;j++){
-        if(nums[i]!=nums[j]){
-            nums[++i]=nums[j];
+
+        // k points to the last unique element index
+        int k = 0;
+
+        // j scans the array from start to end
+        for (int j = 0; j < nums.length; j++) {
+
+            // if current element is different from last kept one
+            if (nums[k] != nums[j]) {
+
+                // move k forward and place the new unique element
+                nums[++k] = nums[j];
+            }
         }
-       }
-       return ++i;
+
+        // length of array with unique elements
+        return ++k;
     }
 }
